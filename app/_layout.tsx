e5fc1993cff5b7b58dadf96104e889bf2b9e11d5
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-14 23:16:25 +0200

    feat: using dark and light mode depending on system settings automatically added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,9 @@
 import { Text, View } from 'react-native';
 import '@/styles/nativewind.css';
-
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: '#f3f4f6',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text className="bg-slate-400">valnir</Text>
+    <View className="items-center justify-center flex-1 bg-white dark:bg-black ">
+      <Text className="color-black dark:color-white">valnir</Text>
     </View>
   );
 };
