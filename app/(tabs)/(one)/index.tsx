e49a81bed49ff3d4b6322f340c528276c1ccb79d
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-17 00:22:29 +0200

    chore: remove husky and lint-staged configurations

--- app/(tabs)/(one)/index.tsx
+++ app/(tabs)/(one)/index.tsx
@@ -1,7 +1,6 @@
 import { View, Text } from 'react-native';
 
 const OneScreen = () => {
-  const x = 1;
   return (
     <View className="items-center justify-center flex-1 bg-white dark:bg-black">
       <Text className="color-black dark:color-white">One</Text>
