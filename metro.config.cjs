d59044202ab90d3c71cb441f0cc1a08e86993f71
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-16 23:13:33 +0200

    chore: add blockList to exclude test files in metro config

--- metro.config.cjs
+++ metro.config.cjs
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.blockList = [
+  /.*\.test\.(ts|tsx)$/, // Exclude test files
+];
+
 module.exports = withNativeWind(config, { input: './nativewind.css' });
