3e64e18c2efc3a50a4113839e510985204346447
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-17 00:12:42 +0200

    feat: eslint configuration adjustments

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,33 @@
+import parser from '@typescript-eslint/parser';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 
 export default [
-  eslintPluginUnicorn.configs.all,
   {
-    ignores: ['.gitignore'],
     files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+    },
     rules: {
+      'no-unused-vars': 'error',
       'unicorn/better-regex': 'warn',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ];
