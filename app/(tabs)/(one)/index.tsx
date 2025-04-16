60f276e747c9e3dc703903ccad569e19fe27a4a2
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-16 23:33:29 +0200

    chore: husky test

--- app/(tabs)/(one)/index.tsx
+++ app/(tabs)/(one)/index.tsx
@@ -3,7 +3,6 @@ import { View,
 
 const OneScreen = () => {
   const x = 1
-  const y = 2
   return (
     <View className="items-center justify-center flex-1 bg-white dark:bg-black">
       <Text className="color-black dark:color-white">One</Text>
