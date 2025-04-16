51a18d0a39b9ac27be7cd118bc208cc57b4c8e50
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-17 01:11:46 +0200

    feat: simplify language resource keys and update language detection

--- i18n/index.ts
+++ i18n/index.ts
@@ -5,18 +5,15 @@ import translationDe from './de.json';
 import translationEn from './en.json';
 
 const resources = {
-  'de-DE': { translation: translationDe },
-  'en-US': { translation: translationEn },
+  de: { translation: translationDe },
+  en: { translation: translationEn },
 };
 
 const initI18n = async () => {
   i18n.use(initReactI18next).init({
     resources,
-    lng: Localization.getLocales()[0].languageTag,
-    fallbackLng: 'de-DE',
-    interpolation: {
-      escapeValue: false,
-    },
+    lng: Localization.getLocales()[0].languageCode || 'en',
+    interpolation: { escapeValue: false },
   });
 };
 
