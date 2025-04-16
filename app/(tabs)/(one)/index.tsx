c7d92aeef7522f1bf163075a22ae4c810e9e0d4c
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-17 00:19:47 +0200

    chore: test husky

--- app/(tabs)/(one)/index.tsx
+++ app/(tabs)/(one)/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 
 const OneScreen = () => {
-  const x = 2;
+  const x = 3;
   return (
     <View className="items-center justify-center flex-1 bg-white dark:bg-black">
       <Text className="color-black dark:color-white">One</Text>
