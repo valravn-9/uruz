67fc8742e36608c2e2579a068b1c400949f7e5a9
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-17 23:25:52 +0200

    feat: added themes to tailwind config and prepare components folder in jest

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ export default {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
   ],
   setupFilesAfterEnv: ['./tests/setup.ts'],
+  moduleNameMapper: {
+    '^components$': '<rootDir>/components',
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,34 @@
 import nativewindPreset from 'nativewind/preset';
 
 export default {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [nativewindPreset],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#3b82f6',
+          dark: '#2563eb',
+        },
+        secondary: {
+          light: '#fbbf24',
+          dark: '#f59e0b',
+        },
+        background: {
+          light: '#ffffff',
+          dark: '#000000',
+        },
+        text: {
+          light: '#000000',
+          dark: '#ffffff',
+        },
+      },
+      fontSize: {
+        base: '16px',
+        lg: '20px',
+        xl: '24px',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
