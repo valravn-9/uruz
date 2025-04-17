ea05b23ffad6b1a848191f27987bb2d99bc0fbf9
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-18 00:22:42 +0200

    feat: add colors as constants and use in tailwind config

---
+++ constants/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primary: {
+    light: '#3b82f6',
+    dark: '#2563eb',
+  },
+  secondary: {
+    light: '#fbbf24',
+    dark: '#f59e0b',
+  },
+  background: {
+    light: '#ffffff',
+    dark: '#000000',
+  },
+  text: {
+    light: '#000000',
+    dark: '#ffffff',
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import nativewindPreset from 'nativewind/preset';
+import { colors } from './constants';
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
@@ -7,22 +8,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          light: '#3b82f6',
-          dark: '#2563eb',
-        },
-        secondary: {
-          light: '#fbbf24',
-          dark: '#f59e0b',
-        },
-        background: {
-          light: '#ffffff',
-          dark: '#000000',
-        },
-        text: {
-          light: '#000000',
-          dark: '#ffffff',
-        },
+        primary: colors.primary,
+        secondary: colors.secondary,
+        background: colors.background,
+        text: colors.text,
       },
       fontFamily: {
         branda: ['Branda', 'sans-serif'],
